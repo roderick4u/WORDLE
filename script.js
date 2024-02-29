3fc5d03314e87d0facf19ffa4cb033bccac29586
roderick4u <160611827+roderick4u@users.noreply.github.com>
2024-02-28 21:46:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,42 @@
-// Seleccionar elementos del DOM
 const guess_word = document.getElementById("guess-input");
 const button = document.getElementById("guess-button");
 const letterContainers = document.getElementById("correct-letters");
 const buttonReset = document.getElementById("reset-button");
 const buttonGiveUp = document.getElementById("give-up-button");
 
-// Definir variables
 let intentos = 6;
-let wordAPI = "APPLE"; // Palabra aleatoria (sin el uso de la API)
+let wordAPI = "APPLE"; 
 let correctLetters = 0;
 
-// Función para reiniciar el juego
 buttonReset.addEventListener("click", () => {
     location.reload();
 });
 
-// Función para rendirse y mostrar la palabra
 buttonGiveUp.addEventListener("click", () => { 
     alert("La palabra era: " + wordAPI);
 });
 
-// Función para verificar la conjetura del usuario
 button.addEventListener("click", checkGuess);
 
 function checkGuess() {
-    // Verificar si se han agotado los intentos
     if (intentos === 0) {
         alert("Perdiste");
         return;
     }
 
-    // Verificar si el usuario ha adivinado todas las letras
     verification(correctLetters);
 
-    // Obtener la conjetura del usuario
     let word = guess_word.value.toUpperCase();
     let arrayWord = word.split('');
     let arrayDictionaryWord = wordAPI.split('');
 
-    // Verificar si la palabra tiene la longitud correcta
+
     if (word.length !== 5) {
         alert("La palabra debe tener 5 letras");
         return;
     }
 
-    // Comparar letras
+
     for (let i = 0; i < arrayDictionaryWord.length; i++) {
         const letterContainer = document.createElement("div");
         letterContainer.classList.add("letter-container");
@@ -62,17 +54,14 @@ function checkGuess() {
         letterContainers.appendChild(letterContainer);
     }
 
-    // Agregar salto de línea después de mostrar las letras
+
     letterContainers.innerHTML += "<br>";
 
-    // Reducir el contador de intentos
+
     intentos--;
 
-    // Verificar si se han adivinado todas las letras
     verification(correctLetters);
-}
 
-// Función para verificar si se han adivinado todas las letras
 function verification(correctLetters) {
     if (correctLetters === 5) {
         alert("Ganaste");
